db74dab71266cac17e137e9ff60547c97f4d4b53
YUAN YI <151559241+Leonildayyy@users.noreply.github.com>
2025-09-29 15:42:40 +0200

    Update solution.sql

--- 534. Game Play Analysis III/solution.sql
+++ 534. Game Play Analysis III/solution.sql
@@ -1,1 +1,11 @@
-
+/* 
+Use SUM() OVER (PARTITION BY player_id ORDER BY event_date) 
+to calculate the running total of games for each player.
+This ensures we get a cumulative value up to that day 
+(instead of the overall total).
+*/
+SELECT
+    player_id,
+    event_date,
+    SUM(games_played) OVER(PARTITION BY player_id ORDER BY event_date) AS games_played_so_far
+FROM Activity
