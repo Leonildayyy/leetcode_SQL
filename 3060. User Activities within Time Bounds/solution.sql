bf8f69c9235c11581b03dd4f4c2fd5693cc27775
YUAN YI <151559241+Leonildayyy@users.noreply.github.com>
2025-10-18 20:02:55 +0200

    Update solution.sql

--- 3060. User Activities within Time Bounds/solution.sql
+++ 3060. User Activities within Time Bounds/solution.sql
@@ -2,7 +2,7 @@
 Question : Write a solution to find the the users who have had at least two session of the same type (either 'Viewer' or 'Streamer') with a maximum gap of 12 hours between sessions.
 Logic: 
   1. For each user and session type, find the previous session’s end time using LAG(session_end).
-  2. LAG(session_end)
+  2. Select the users whose next session starts within 12 hours after their previous one.
 '''
 
 WITH session_diff  AS (
